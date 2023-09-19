a943a84ac1968e932bda81aa34f61f8140285ced
Kyankeise <122470088+Kyankeise@users.noreply.github.com>
2023-09-19 12:21:06 +0100

    Update main.js

--- Starbucks-landing-page/HTML /js/main.js
+++ Starbucks-landing-page/HTML /js/main.js
@@ -1,9 +1,14 @@
 // Author: Kyan Keise
-// Date:
-// Title: 
+// Date: 06/05/22
+// Title: Starbucks landing page
 
+
+// Declare Vaiables 
 const btn = document.getElementById('menu-btn')
 const nav = document.getElementById('menu')
+
+
+// Controls the side Navigation Menu
 
 function navToggle() {
 btn.classList.toggle('open')
